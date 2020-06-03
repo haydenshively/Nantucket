38425d18a0280cc277ff7716b4fcc3b06511b2b1
Hayden Shively <haydenshively@gmail.com>
2020-06-03 18:10:26 -0500

    Project config and files from AAVE truffle-box

---
+++ contracts/aave/IFlashLoanReceiver.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GNU
+pragma solidity ^0.6.6;
+
+/**
+* @title IFlashLoanReceiver interface
+* @notice Interface for the Aave fee IFlashLoanReceiver.
+* @author Aave
+* @dev implement this interface to develop a flashloan-compatible flashLoanReceiver contract
+**/
+interface IFlashLoanReceiver {
+    function executeOperation(address _reserve, uint256 _amount, uint256 _fee, bytes calldata _params) external;
+}
---
+++ contracts/aave/ILendingPoolAddressesProvider.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GNU
+pragma solidity ^0.6.6;
+
+/**
+    @title ILendingPoolAddressesProvider interface
+    @notice provides the interface to fetch the LendingPoolCore address
+ */
+
+interface ILendingPoolAddressesProvider {
+    function getLendingPoolCore() external view returns (address payable);
+    function getLendingPool() external view returns (address);
+}
