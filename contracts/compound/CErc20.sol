71e1d23245861f498170c2c466158375d94b0ff2
Hayden Shively <haydenshively@gmail.com>
2020-06-14 01:37:26 -0500

    Added Compound interfaces

---
+++ contracts/compound/CErc20.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.6;
+
+interface CErc20 {
+    function mint(uint mintAmount) external returns (uint);
+    function redeem(uint redeemTokens) external returns (uint);
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
+    function borrow(uint borrowAmount) external returns (uint);
+    function repayBorrow(uint repayAmount) external returns (uint);
+    function repayBorrowBehalf(address borrower, uint repayAmount) external returns (uint);
+    function liquidateBorrow(address borrower, uint repayAmount, address collateral) external returns (uint);
+}
