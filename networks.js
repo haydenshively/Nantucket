ea423a664b6ad6bfadeac2827d5b7b0849e7cad8
Hayden Shively <haydenshively@gmail.com>
2020-06-17 04:38:45 -0500

    Added config for kovan deployment

--- networks.js
+++ networks.js
@@ -5,11 +5,9 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   networks: {
     development: {
-      protocol: 'http',
-      host: 'localhost',
-      port: 8545,
+      provider: () => new HDWalletProvider('glad famous milk doctor wedding rifle piece rival fancy youth boost frame', 'https://kovan.infura.io/v3/' + process.env.INFURA_ID),
       gas: 5000000,
-      gasPrice: 5e9,
+      gasPrice: 35e9,
       networkId: '*',
     },
     production: {
