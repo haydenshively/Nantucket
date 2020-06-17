6e998edc1c8fe870cdb9cee3d0a19ded2c9c6af8
Hayden Shively <haydenshively@gmail.com>
2020-06-17 03:33:09 -0500

    Add config files for mainnet deployment

--- networks.js
+++ networks.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
@@ -6,6 +10,12 @@ module.exports = {
       port: 8545,
       gas: 5000000,
       gasPrice: 5e9,
+      networkId: '*',
+    },
+    production: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://mainnet.infura.io/v3/' + process.env.INFURA_ID),
+      gas: 5000000,
+      gasPrice: 35e9,
       networkId: '*',
     },
   },
