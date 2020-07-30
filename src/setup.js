ad24f69079bc9b3dbd016aee53727a85e0ae4c88
hayden Shively <haydenshively@gmail.com>
2020-07-29 23:01:43 -0500

    Config via json

---
+++ src/setup.js
@@ -0,0 +1,27 @@
+// configure web3
+const Web3 = require("web3");
+if (process.env.WEB3_PROVIDER.endsWith(".ipc")) {
+  net = require("net");
+  global.web3 = new Web3(process.env.WEB3_PROVIDER, net);
+} else {
+  global.web3 = new Web3(process.env.WEB3_PROVIDER);
+}
+
+// configure winston
+const winston = require("winston");
+const SlackHook = require("../src/logging/slackhook");
+winston.configure({
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console({ handleExceptions: true }),
+    new SlackHook({
+      level: "info",
+      webhookUrl: process.env.SLACK_WEBHOOK,
+      mrkdwn: true
+    })
+  ],
+  exitOnError: false
+});
