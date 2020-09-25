7536cc23261e94073c24b63904bd7d81ac567b0f
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2020-09-24 19:07:21 -0500

    Add wallet addresses to geth txpool.locals (#87)

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -i
 
 tmux new-session -d -s geth
-tmux send-keys -t geth "geth --cache 7000 --rpc.txfeecap 10" ENTER
+tmux send-keys -t geth "geth --config /home/haydenshively/Developer/Nantucket/geth.toml" ENTER
 
 tmux new-session -d -s nantucket
 tmux send-keys -t nantucket "sleep 60 && yarn --cwd /home/haydenshively/Developer/Nantucket/ nantucket" ENTER
