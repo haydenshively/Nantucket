9ce3cf6e218204ae97791d138a2075d1d225017a
hayden Shively <haydenshively@gmail.com>
2020-10-30 22:17:58 -0500

    Remove 'Transaction not mined within...' logs

--- src/network/webthree/providers.js
+++ src/network/webthree/providers.js
@@ -93,7 +93,7 @@ class MultiSendProvider {
     for (let i = 1; i < this.providers.length; i++)
       this.providers[i].eth
         .sendSignedTransaction(signedTx)
-        .on("error", console.error);
+        .on("error", console.log);
     return sentTx;
   }
 
