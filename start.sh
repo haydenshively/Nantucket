b892d866d4124fe69443c00544fb6065767a164c
hayden Shively <haydenshively@gmail.com>
2021-04-13 23:32:19 -0500

    Cleanup for public release

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -i
-
-tmux new-session -d -s geth
-tmux send-keys -t geth "geth --config /home/haydenshively/Developer/Nantucket/geth.toml" ENTER
-
-tmux new-session -d -s nantucket
-tmux send-keys -t nantucket "sleep 60 && yarn --cwd /home/haydenshively/Developer/Nantucket/ nantucket" ENTER
